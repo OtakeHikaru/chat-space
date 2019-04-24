f6eb1a37cc2864887a0b97a3068e3cbe07560da4
OtakeHikaru <yellowtake59@icloud.com>
2019-04-24 16:15:35 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ChatSpace
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascriprs false
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
+
+
+
+
