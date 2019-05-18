e1237af0fc936c1013372b078df28f391b99f6d0
OtakeHikaru <yellowtake59@icloud.com>
2019-05-18 15:11:14 +0900

    Create message index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-
+  before_action :set_group, only: [:edit, :update]
   def index
   end
 
@@ -19,7 +19,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redurect_to group_messages_path(@group), notice: 'グループを編集しました'
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
@@ -27,7 +27,7 @@ class GroupsController < ApplicationController
   
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => {} })
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 
   def set_group
