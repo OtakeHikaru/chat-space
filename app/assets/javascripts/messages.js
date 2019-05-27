603dedb84fbeabdb9be76237f5f549ceaf872519
OtakeHikaru <yellowtake59@icloud.com>
2019-05-27 21:32:39 +0900

    Edit messages.js
    Edit html

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,30 +1,30 @@
 $(function(){
   function buildHTML(message){
     var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="message-text__image">`
-    var html = `<div class="message>
-                  <div class="user-info>
-                    <div class="user-info__user-name">
-                      ${message.user_name}
+    var html = `  <div class="message">
+                    <div class="user-info">
+                      <div class="user-info__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="user-info__date">
+                        ${message.created_at}
+                      </div>
                     </div>
-                    <div class="user-info__date">
-                      ${message.created_at}
+                    <div class="message-text"
+                      <p class="message-text__content">
+                        ${message.content}
+                      </p>
+                      ${imagehtml}
                     </div>
-                  </div>
-                  <div class="message-text"
-                    <p class="message-text__content">
-                      ${message.content}
-                    </p>
-                    ${imagehtml}
-                  </div>
-                </div>`
+                  </div>`
     return html;
   }
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var href = window.location.href
       $.ajax({
-        url: url,
+        url: href,
         type: 'POST',
         data: formData,
         dataType: 'json',
@@ -33,11 +33,11 @@ $(function(){
       })
         .done(function(data) {
           var html = buildHTML(data);
-          $('.message').append(html);
-          $('.form__submit').val('');
+          $('.messages').append(html);
+          $('.form__message').val('');
           $('.hidden').val('');
           $('.form__submit').prop('disabled', false);
-          $('.message').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       })
       .fail(function(){
         alert('error');
