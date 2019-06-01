c1d56b61d6eaff5a91083f8f081f983d0de2371e
OtakeHikaru <yellowtake59@icloud.com>
2019-06-01 10:59:40 +0900

    Create incremental-search

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,58 @@
+$(function() {
+  function listUser(user) {
+    var html =  `
+                <div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+                `
+    return html;
+  };
+
+  $('#user-search-field').on("keyup", function() {
+    var input = $("#user-search-field").val();
+    var href = window.location.href;
+    
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(data) {
+      $(".user-search-result").empty();
+      if(data.length !== 0) {
+        data.forEach(function(user) {
+          var html = listUser(user);
+          $(".user-search-result").append(html);
+        });
+      }
+    })
+
+    .fail(function(){
+      alert('通信失敗しました');
+    });
+  });
+
+  function clickAdd(user){
+    var userId = user.attr("data-user-id");
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                  <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                  <p class='chat-group-user__name'>${user.attr("data-user-name")}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    return html;
+  };
+
+  $(document).on("click", ".user-search-add", function (){
+    $input = $(this);
+    var add_user_html = clickAdd($input);
+    $(".search-users").append(add_user_html);
+    $input.parent().remove();
+  });
+  $(document).on("click", ".user-search-remove", function() {
+    $input = $(this);
+    $input.parent().remove();
+  });
+});
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20190513041547) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
   end
 
   create_table "installs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -33,6 +34,7 @@ ActiveRecord::Schema.define(version: 20190513041547) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "current_sign_in_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_installs_on_email", unique: true, using: :btree
