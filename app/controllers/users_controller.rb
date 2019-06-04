776fe646d0bc04aa04991aafb0606034f39c0f0a
OtakeHikaru <yellowtake59@icloud.com>
2019-06-04 21:07:27 +0900

    Edit form.html
    ログイン中のユーザーをグループに追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where('name LIKE(?) and id != ?', "%#{params[:keyword]}%" ,current_user)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
